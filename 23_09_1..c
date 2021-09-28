55ef1ca2b5f705f0eea5c064099fc4dc32f24357
mike <michalbaz@poczta.onet.pl>
2021-09-28 15:19:53 +0200

    new file

---
+++ 23_09_1..c
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <stdio.h>
+
+int main(void){
+    
+    //hejka hej to jest komentarz jednolinijkowy
+    /*
+    komentarz wielolinijkowy
+    
+    hej hej
+    co tam
+    
+    XD
+    
+    
+
+    float avc123; //deklaracja
+    float _avc123 = 5.0f; //definicja
+    char c = '\n';
+
+    const int stala = 5; //przy tworzeniu stałej zawsze definicja od razu bo później nie da się nadać jej wartości
+    int zmienna = 10;
+
+
+    //+, -, /, *
+
+    float a = 5.0f, b = 1.0f, c; 
+    int d, e, f;
+
+    
+
+    return 0;
+}*/
